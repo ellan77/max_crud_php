84baaa61a7563cb3a718eed78b9e7438862d8760
Dmitry Smyrnov <smyrnovd@gmail.com>
2019-06-22 12:41:00 +0300

    initial commit

---
+++ config.php
@@ -0,0 +1,16 @@
+<?php
+/* Database credentials. Assuming you are running MySQL
+server with default setting (user 'root' with no password) */
+define('DB_SERVER', 'localhost');
+define('DB_USERNAME', 'appandroid');
+define('DB_PASSWORD', 'pass');
+define('DB_NAME', 'appandroid');
+ 
+/* Attempt to connect to MySQL database */
+$link = mysqli_connect(DB_SERVER, DB_USERNAME, DB_PASSWORD, DB_NAME);
+ 
+// Check connection
+if($link === false){
+    die("ERROR: Could not connect. " . mysqli_connect_error());
+}
+?>

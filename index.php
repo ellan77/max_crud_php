eb36452434ee5530c50a629c34c02db7732fb1c2
Dmitry Smyrnov <smyrnovd@gmail.com>
2019-06-22 13:02:23 +0300

    создана таблица сообщений

--- index.php
+++ index.php
@@ -74,7 +74,72 @@
                             // Free result set
                             mysqli_free_result($result);
                         } else{
-                            echo "<p class='lead'><em>No records were found.</em></p>";
+                            echo "<p class='lead'><em>Нет записей в базе</em></p>";
+                        }
+                    } else{
+                        echo "ERROR: Could not able to execute $sql. " . mysqli_error($link);
+                    }
+ 
+                  
+                   
+                    ?>
+                </div>
+            </div>
+
+
+
+
+
+
+
+
+
+            <div class="row">
+                <div class="col-md-12">
+                    <div class="page-header clearfix">
+                        <h2 class="pull-left">Таблица сообщений</h2>
+                        <a href="create_user.php" class="btn btn-success pull-right">Создать сообщение</a>
+                    </div>
+                    <?php
+                    // Include config file
+                    require_once "config.php";
+                    
+                    // Attempt select query execution
+                    $sql = "SELECT * FROM messages";
+                    if($result = mysqli_query($link, $sql)){
+                        if(mysqli_num_rows($result) > 0){
+                            echo "<table class='table table-bordered table-striped'>";
+                                echo "<thead>";
+                                    echo "<tr>";
+                                        echo "<th>#</th>";
+                                        echo "<th>От пользователя</th>";
+                                        echo "<th>Пользователю</th>";
+                                        echo "<th>Сообщение</th>";
+                                        echo "<th>Дата отправки</th>";
+                                        echo "<th class='control'>CONTROL</th>";
+
+                                    echo "</tr>";
+                                echo "</thead>";
+                                echo "<tbody>";
+                                while($row = mysqli_fetch_array($result)){
+                                    echo "<tr>";
+                                        echo "<td>" . $row['id'] . "</td>";
+                                        echo "<td>" . $row['user_id'] . "</td>";
+                                        echo "<td>" . $row['to_user_id'] . "</td>";
+                                        echo "<td>" . $row['message'] . "</td>";
+                                        echo "<td>" . $row['created'] . "</td>";
+                                        echo "<td>";
+                                            echo "<a href='update_message.php?id=". $row['id'] ."' title='Редактировать сообщение' data-toggle='tooltip'><span class='glyphicon glyphicon-pencil'></span></a>";
+                                            echo "<a href='delete_message.php?id=". $row['id'] ."' title='Удалить сообщение' data-toggle='tooltip'><span class='glyphicon glyphicon-trash'></span></a>";
+                                        echo "</td>";
+                                    echo "</tr>";
+                                }
+                                echo "</tbody>";                            
+                            echo "</table>";
+                            // Free result set
+                            mysqli_free_result($result);
+                        } else{
+                            echo "<p class='lead'><em>Нет записей в базе</em></p>";
                         }
                     } else{
                         echo "ERROR: Could not able to execute $sql. " . mysqli_error($link);
@@ -84,7 +149,8 @@
                     mysqli_close($link);
                     ?>
                 </div>
-            </div>        
+            </div>   
+
         </div>
     </div>
 </body>
